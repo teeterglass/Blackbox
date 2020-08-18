311560055a6186af61db61f6ed017c12ce234247
jteeter <teeterglass@gmail.com>
2020-08-18 14:28:06 -0700

    portf

--- Graphics_classes.py
+++ Graphics_classes.py
@@ -4,36 +4,58 @@ import pygame
 class Button:
     """Class to describe a button"""
 
-    def __init__(self, bb_settings, screen, msg, x_center, y_center, num_atom):
+    def __init__(self, screen, msg, x_center, y_center, num_atom, wide=False):
         """Initialize button attributes."""
-        self.screen = screen
-        self.screen_rect = screen.get_rect()
+        self._screen = screen
+        self._screen_rect = screen.get_rect()
+        if wide:
+            # Set the dimensions and properties of the button.
+            self._width, self._height = 400, 100
+            self._button_color = (0, 0, 140)
+            self._text_color = (255, 255, 255)
+            self._font = pygame.font.SysFont(None, 28)
+            self._num_atom = num_atom
 
-        # Set the dimensions and properties of the button.
-        self.width, self.height = 200, 100
-        self.button_color = (0, 0, 140)
-        self.text_color = (255, 255, 255)
-        self.font = pygame.font.SysFont(None, 28)
-        self.num_atom = num_atom
+            # Build the button's rect object and center it
+            self._rect = pygame.Rect(0, 0, self._width, self._height)
+            self._rect.center = (x_center, y_center)
 
-        # Build the button's rect object and center it
-        self.rect = pygame.Rect(0, 0, self.width, self.height)
-        self.rect.center = (x_center, y_center)
+            # The button message needs to be prepped only once
+            self.prep_msg(msg)
+        else:
+            # Set the dimensions and properties of the button.
+            self._width, self._height = 200, 100
+            self._button_color = (0, 0, 140)
+            self._text_color = (255, 255, 255)
+            self._font = pygame.font.SysFont(None, 28)
+            self._num_atom = num_atom
 
-        # The button message needs to be prepped only once
-        self.prep_msg(msg)
+            # Build the button's rect object and center it
+            self._rect = pygame.Rect(0, 0, self._width, self._height)
+            self._rect.center = (x_center, y_center)
+
+            # The button message needs to be prepped only once
+            self.prep_msg(msg)
 
     def prep_msg(self, msg):
         """msg is not a rendered image and center text on the button"""
-        self.msg_image = self.font.render(msg, True, self.text_color,
-                                          self.button_color)
+        self.msg_image = self._font.render(msg, True, self._text_color,
+                                          self._button_color)
         self.msg_image_rect = self.msg_image.get_rect()
-        self.msg_image_rect.center = self.rect.center
+        self.msg_image_rect.center = self._rect.center
 
     def draw_button(self):
         """Draw a blank button and then draw message"""
-        self.screen.fill(self.button_color, self.rect)
-        self.screen.blit(self.msg_image, self.msg_image_rect)
+        self._screen.fill(self._button_color, self._rect)
+        self._screen.blit(self.msg_image, self.msg_image_rect)
+
+    def get_button_rect(self):
+        """return button rectangle"""
+        return self._rect
+
+    def get_num_atom(self):
+        """return atom number"""
+        return self._num_atom
 
 
 class Marker:
