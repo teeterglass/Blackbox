9a6d30bbb7004529d821af0f4d295cc4ff7432f8
jteeter <teeterglass@gmail.com>
2020-08-18 13:56:36 -0700

    updated file

--- Graphics_classes.py
+++ Graphics_classes.py
@@ -40,21 +40,75 @@ class Marker:
     """Class to model a pair of markers for the entry, exit locations/atoms"""
     def __init__(self, color, screen):
         """initialization of marker variables"""
-        self.screen = screen
-        self.screen_circle = screen.get_rect()
-        self.circle_center = (0, 0), (0, 0)
-        self.circle_color = color
+        self._screen = screen
+        self._screen_circle = self._screen.get_rect()
+        self._circle_center = (0, 0), (0, 0)
+        self._circle_color = color
         # Build the button's rect object and center it
-        self.circle_rad = 30
+        self._circle_rad = 30
 
     def draw_marker(self):
         """Draw a blank marker(s) for entry/exit/atom"""
-        pygame.draw.circle(self.screen, self.circle_color, self.circle_center[0],
-                           self.circle_rad)
-        if self.circle_center[1] != (0,0):
-            pygame.draw.circle(self.screen, self.circle_color,
-                               self.circle_center[1], self.circle_rad)
+        pygame.draw.circle(self._screen, self._circle_color,
+                           self._circle_center[0], self._circle_rad)
+        if self._circle_center[1] != (0, 0):
+            pygame.draw.circle(self._screen, self._circle_color,
+                               self._circle_center[1], self._circle_rad)
 
     def update_center(self, center_entry, center_exit=(0, 0)):
         """updates center once marker has been assigned"""
-        self.circle_center = center_entry, center_exit
+        self._circle_center = center_entry, center_exit
+
+
+class Scoreboard:
+    """class to describe the score board operations"""
+
+    def __init__(self, bb_settings, screen):
+        self._bb_settings = bb_settings
+        self._screen = screen
+        self._screen_rect = self._screen.get_rect()
+        self.prep_score_board()
+
+    def prep_score_board(self):
+        """Turn the score into a rendered image."""
+
+        current_score = "Lets get started!"
+        num_atoms = "Atoms to be found!"
+        self._score_image = self._bb_settings.font.render(current_score, True,
+                                             self._bb_settings.text_color,
+                                             self._bb_settings.bg_color)
+
+        self._atom_image = self._bb_settings.font.render(num_atoms, True,
+                                             self._bb_settings.text_color,
+                                             self._bb_settings.bg_color)
+        # Display the score at the top right of the screen.
+        self._score_rect = self._score_image.get_rect()
+        self._score_rect.right = self._screen_rect.right - 20
+        self._score_rect.top = 20
+
+        self._atom_rect = self._atom_image.get_rect()
+        self._atom_rect.right = self._screen_rect.right - 20
+        self._atom_rect.top = 80
+
+    def get_score_image_rect(self, points, atom_left):
+        """returns score image and rect"""
+        current_score = str(points) + " Points"
+        if points <= 0:
+            num_atoms = "You lost!"
+        elif atom_left > 0:
+            num_atoms = str(atom_left) + " Atoms left"
+        else:
+            num_atoms = "You Won!"
+
+        self._score_image = self._bb_settings.font.render(current_score, True,
+                                             self._bb_settings.text_color,
+                                             self._bb_settings.bg_color)
+
+        self._atom_image = self._bb_settings.font.render(num_atoms, True,
+                                             self._bb_settings.text_color,
+                                             self._bb_settings.bg_color)
+
+        return self._score_image, self._score_rect, \
+               self._atom_image, self._atom_rect
+
+
