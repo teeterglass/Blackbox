930cb19a5c921b8b4d89a139f9c5098f8751b8d4
jteeter <teeterglass@gmail.com>
2020-08-18 13:56:47 -0700

    portf

--- settings.py
+++ settings.py
@@ -23,6 +23,36 @@ class GameStats():
 
     def __init__(self, bb_settings):
         """Initialize statistics."""
-        self.bb_settings = bb_settings
-        self.game_active = False
+        self._bb_settings = bb_settings
+        self._game_active = "Start_game"
+        self._points = 25
+        self._atom_left = 0
+
+    def set_status(self, new_value):
+        """set status to a new value"""
+        self._game_active = new_value
+
+    def get_status(self):
+        """return status of game"""
+        return self._game_active
+
+    def update_num_atoms(self, num_atoms):
+        """recieve initial number of atoms"""
+        self._atom_left = num_atoms
+
+    def get_num_atoms_left(self):
+        """return number of atoms left to be guessed"""
+        return self._atom_left
+
+    def remove_atom(self):
+        """removes atom from list if guessed right"""
+        self._atom_left -= 1
+
+    def get_points(self):
+        """returns the current point total for a player"""
+        return self._points
+
+    def dec_player_score(self, count):
+        """decrements player's score by given count"""
+        self._points -= count
 
